c4fca7202e7394655e9dd428359105d8c7d3bc19
Felix Bachmann <felix.bachmann@ewetel.net>
2020-03-24 12:08:21 +0100

    now auto-starting live ping plot on mn startup; now ignoring csv data

--- mn/startup/start_topo.py
+++ mn/startup/start_topo.py
@@ -5,6 +5,7 @@ import time
 import threading
 
 import argparse
+import subprocess
 
 from mininet.net import Containernet
 from mininet.link import TCLink
@@ -19,8 +20,14 @@ def printAndExecute(host, cmdString):
     host.cmd(cmdString)
 
 def startARP(host, srcIP, srcMAC, dstIP, iface):
-    cmdString = 'python /home/felix/Desktop/ba/code/mn/hosts/cli/ARP/ARP_client.py \"%s\" %s %s %s' % (srcIP, dstIP, srcMAC, iface)
+    cmdString = 'python /home/felix/Desktop/toposync/mn/ARP_client.py \"%s\" %s %s %s' % (srcIP, dstIP, srcMAC, iface)
     printAndExecute(host, cmdString)
+
+def startPing(host):
+    cmdString = 'python /home/felix/Desktop/toposync/mn/ping/ping.py &'
+    printAndExecute(host, cmdString)
+
+
 
 def main():
     parser = argparse.ArgumentParser(description='Starts a containernet topology and a REST server for VNF instantiation from controller.')
@@ -42,8 +49,6 @@ def main():
 
     setLogLevel('info')
 
-
-
     net = Containernet(controller=RemoteController, topo=topo, build=False, autoSetMacs=True, link=TCLink)
     net.start()
 
@@ -58,13 +63,24 @@ def main():
     hosts = net.hosts
 
     # send arp from reqHost to every other host -> required by ONOS HostService to resolve hosts (i.e. map MAC<->IP)
+    print('**Starting to ARP for ONOS host discovery.')
     reqHost = hosts[0]
     for host in hosts:
         if(host is not reqHost):
             startARP(reqHost, reqHost.IP(), reqHost.MAC(), host.IP(), reqHost.intf())
 
+    # start to ping from server to clients
+    print('**Starting to ping from server to clients.')
+    shost = net.getNodeByName('shost')
+    startPing(shost)
+
+    # start to plot ping results
+    print('**Starting to plot ping results.')
+    p = subprocess.Popen(['python','/home/felix/Desktop/toposync/mn/ping/plotter.py', '&'])
+
     CLI(net)
 
+    p.kill()
     net.stop()
 
 if __name__ == '__main__':
