d23174a13cd8abc954bddf50f2bd3cafcfaf20e1
Felix Bachmann <felix.bachmann@ewetel.net>
2020-04-10 12:07:38 +0200

    implemented VNF for whack a mole transcoding

--- mn/Sniffer.py
+++ mn/Sniffer.py
@@ -8,6 +8,7 @@ import csv
 from struct import *
 from scapy.all import *
 import threading
+import xml.etree.ElementTree as ET
 
 #Convert a string of 6 characters of ethernet address into a dash separated hex string
 def eth_addr (a) :
@@ -29,6 +30,51 @@ def debug_send(pkt):
     if args.v:
         print ("sending now!")
     scapy_sock.send(pkt)
+
+def ttl_mac(vnf_name):
+    ether_dst = None
+    ttl = None
+    if args.name == "TRANSCODER" or args.name == "TRANSCODER_accelerated":
+        ether_dst = "22:22:22:22:22:22"
+        ttl = 42
+    elif args.name == "INTRUSION_DETECTION":
+        ether_dst = "33:33:33:33:33:33"
+        ttl = 43
+    return (ttl, ether_dst)
+
+def forward_delayed(pkt, delay, reception_time):
+    #print("from receive to before send: %s" % (int(round(time.time() * 1000)) - reception_time))
+    before_send = int(round(time.time() * 1000))
+    if before_send - reception_time >= args.delay: # if delay by unpacking the packet already exceeds the delay, instantly send the packet back
+        #print("sending packet right away")
+        scapy_sock.send(pkt)
+    else:
+        #print("scheduling sending for in %s seconds." % str((args.delay - (before_send - reception_time)) / 1000.0))
+        t = threading.Timer((args.delay - (before_send - reception_time)) / 1000.0, debug_send, args=[pkt])
+        t.start()
+
+def transcode_to_csv(xml_data):
+    csv_string = ""
+    if args.v:
+        print('need to transcode %s' % xml_data)
+    root = ET.fromstring(xml_data)
+    for idx, child in enumerate(root):
+        if child.tag == 'mole':
+            row, col = (child.attrib['row'], child.attrib['col'])
+            if args.v:
+                print('row: %s' % row)
+                print('col: %s' % col)
+                print('...')
+            csv_string += (row + ',' + col)
+            if idx != len(root)-1:
+                csv_string += '\n'
+    
+    if args.v:
+        print('transcoded:\n\"%s\"' % csv_string)
+
+    return csv_string
+
+
 
 while True:
     packet = s.recvfrom(10000)
@@ -78,36 +124,12 @@ while True:
 
         if str(ip_dst) == "10.0.0.1": # ping from receiver back to source
             #print("directly forwarding reverse ping")
-            if args.name == "TRANSCODER" or args.name == "TRANSCODER_accelerated":
-                ttl = 42
-            elif args.name == "INTRUSION_DETECTION":
-                ttl = 43
+            ttl,_ = ttl_mac(args.name)
             pkt = Ether(dst=eth_dst)/IP(dst=d_addr, src=s_addr, ttl=ttl, proto=protocol)/Raw(load=ip_data)
             scapy_sock.send(pkt)
 
-        if ip_dst.is_multicast:     
-            #print("from receive to before send: %s" % (int(round(time.time() * 1000)) - reception_time))
-            ether_dst = ""
-            ttl = 0
-            if args.name == "TRANSCODER" or args.name == "TRANSCODER_accelerated":
-                ether_dst = "22:22:22:22:22:22"
-                ttl = 42
-            elif args.name == "INTRUSION_DETECTION":
-                ether_dst = "33:33:33:33:33:33"
-                ttl = 43
-
-            pkt = Ether(dst=ether_dst)/IP(dst=d_addr, src=s_addr, ttl=ttl, proto=protocol)/Raw(load=ip_data)
-            before_send = int(round(time.time() * 1000))
-            if before_send - reception_time >= args.delay: # if delay by unpacking the packet already exceeds the delay, instantly send the packet back
-                #print("sending packet right away")
-                scapy_sock.send(pkt)
-            else:
-                #print("scheduling sending for in %s seconds." % str((args.delay - (before_send - reception_time)) / 1000.0))
-                t = threading.Timer((args.delay - (before_send - reception_time)) / 1000.0, debug_send, args=[pkt])
-                t.start()
-
         ## UDP
-        elif protocol == 17:
+        if protocol == 17:
             u = iph_length + eth_length
             udph_length = 8
             udp_header = packet[u:u+8]
@@ -127,6 +149,24 @@ while True:
             data = packet[h_size:]
             if args.v:
                 print("UDP[SrcPort: %s, DstPort: %s, Len: %s, Checksum: %s, Data: %s" % (source_port, dest_port, length, hex(checksum), binascii.hexlify(data)))
+            
+            if dest_port == 9090:
+                if args.v:
+                    print('whack-a-mole packet!')
+
+                ttl, mac_dst = ttl_mac(args.name)
+
+                transcoded_data = transcode_to_csv(str(data))
+
+                pkt = Ether(dst=mac_dst)/IP(dst=d_addr, src=s_addr, ttl=ttl, proto=protocol)/UDP(sport=source_port,dport=dest_port)/Raw(load=transcoded_data)
+
+                forward_delayed(pkt, args.delay, reception_time)
+                
+
+        elif ip_dst.is_multicast: # just delay everything else sent towards the group
+            ttl, mac_dst = ttl_mac(args.name)
+            pkt = Ether(dst=mac_dst)/IP(dst=d_addr, src=s_addr, ttl=ttl, proto=protocol)/Raw(load=ip_data)
+            forward_delayed(pkt, args.delay, reception_time)
 
         # protocol other than UDP
         else :
